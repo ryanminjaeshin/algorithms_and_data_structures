878a7830e8d387c07f1b3529e515296d7384aa91
Ryan Shin <ryanminjaeshin@gmail.com>
2020-11-16 12:20:14 -0800

    Plan out

--- data_structures/reverse_linked_list_leetcode_206.js
+++ data_structures/reverse_linked_list_leetcode_206.js
@@ -7,6 +7,10 @@ Example: Input: 1->2->3->4->5->NULL
          Output: 5->4->3->2->1->NULL
 */
 
+// Plan
+// Create empty ll
+// attach node to the ll in reverse order.
+
 var reverseList = function(head) {
 
 };
