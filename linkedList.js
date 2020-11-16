6db23ed601d15c511a96ba4a13c6d78d61f8a8f5
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-15 17:07:41 -0800

    Add linkedList class

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,12 @@ class Node {
     }
 }
 
-const node = new Node(100);
+// const node = new Node(100);
+// console.log(node);
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+}
