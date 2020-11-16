d5b11e06b5702200a3e2fa41249ba1c94828ff04
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-16 14:31:45 -0800

    Create climbing_stairs and Add IOCE

---
+++ algorithms/climbing_stairs.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+/**
+ 
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ */
+
+ /**
+  * I: integer
+  * O: integer (number of way)
+  * C: None
+  * Edge case: input 0 or negative
+  * Example: input 3 -> output (1,1,1) , (2,1), (1, 2) = 3
+  */
+
+
+var climbStairs = function(n) {
+     
+};
