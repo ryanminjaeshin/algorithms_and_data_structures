30b93a3cf4ec8917a95cc865ad5b3480a2ad45cf
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-16 17:54:17 -0800

    Pseudo code

---
+++ algorithms/two_sums_leetcode_1.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/two-sum/
+
+/*
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ */
+
+/**
+ * I: number of arrays, and target integer;
+ * O: indexes of two numbers
+ * C: ?
+ * Edge case: none
+ * Example: [1,2,3,4], 7 -> [2,3]
+ */
+var twoSum = function(nums, target) {
+    // create empty array;
+
+    // iterate over the nums array
+    //  push i to empty array
+    //  subtract array[i] from target
+    //  find the indexof above value.
+    //  if it is -1, do nothing
+    //  if it is not -1, push to the array and return
+
+    
+};
