ffb8ba0ada89abdec7d99679e3d4e4570c441853
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-16 17:38:17 -0800

    Finish queue

---
+++ data_structures/queue.js
@@ -0,0 +1,21 @@
+class Queue {
+    constructor() {
+        this.storage = {};
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    enqueue(element) {
+        this.storage[this.tail] = element;
+        this.tail ++;
+    }
+
+    dequeue() {
+        var removed = this.storage[this.head];
+        delete this.storage[this.head];
+        this.head ++;
+        return removed;
+    }
+}
+
+var queue = new Queue();
