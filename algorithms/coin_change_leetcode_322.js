8ef8b31ce98772ba06c82c4d6a75e126efc0c971
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-18 21:25:30 -0800

    Add IOCE

--- algorithms/coin_change_leetcode_322.js
+++ algorithms/coin_change_leetcode_322.js
@@ -6,6 +6,38 @@ You are given coins of different denominations and a total amount of money amoun
 
 You may assume that you have an infinite number of each kind of coin.
 
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+Example 4:
+
+Input: coins = [1], amount = 1
+Output: 1
+Example 5:
+
+Input: coins = [1], amount = 2
+Output: 2
+
+*/
+
+/*
+
+I: array, integer
+O: integer
+C: None
+Edge case: None
+Example: [1,2,3], 6 -> 2
+
 */
 
 var coinChange = function(coins, amount) {
