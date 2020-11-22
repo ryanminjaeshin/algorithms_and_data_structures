8e6b82af887c51e974cbc14ad2f406cd17d2bb79
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-21 16:06:11 -0800

    Finish leetcode 509

--- algorithms/fibonacci_leetcode_509.js
+++ algorithms/fibonacci_leetcode_509.js
@@ -27,3 +27,15 @@ Edge case: When input is negative number, return null;
 Example: (1) -> 1, (3) -> 2
 
 */
+
+
+var fib = (n) => {
+    var len = n + 1;
+    var dp = new Array(len);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (var i = 2; i < len; i ++) {
+        dp[i] = dp[i - 2] + dp[i - 1];
+    }
+    return dp[n]
+}
