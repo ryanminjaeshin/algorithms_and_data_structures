877771f6e69f5faea1234fc9fd320c2872b5aab7
Ryan Shin <ryanminjaeshin@gmail.com>
2020-11-24 12:50:58 -0800

    Add insert method

--- data_structures/binary_search_tree.js
+++ data_structures/binary_search_tree.js
@@ -5,3 +5,45 @@ class Node {
     this.right = null;
   }
 }
+
+class BTS {
+  constructor(value) {
+    this.root = new Node(value);
+    this.size = 1;
+  }
+
+  size() {
+    return this.count ++;
+  }
+
+  insert(value) {
+    this.count ++;
+
+    let newNode = new Node(value);
+
+    const searchTree = node => {
+      // if value < node.value, go left
+      if (value < node.value) {
+        // if no left child, append new node
+        if (!node.left) {
+          node.left = newNode;
+        } else {
+          // if left child exists, look left again
+          searchTree(node.left);
+        }
+
+      // if value > node.value, go right
+      } else if (value > node.value) {
+        // if no right child, append new node
+        if (!node.right) {
+          node.right = newNode;
+        } else {
+          // if right child exists, look right again
+          searchTree(node.right);
+        }
+      }
+    }
+
+    searchTree(this.root)
+  }
+}
