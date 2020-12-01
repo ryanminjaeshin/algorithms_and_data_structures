dba3c2ae440b72973b30822dce3654756c5a7c58
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-12-01 13:45:10 -0800

    Final ver

--- algorithms/sorting_quicksort.js
+++ algorithms/sorting_quicksort.js
@@ -1,16 +1,16 @@
 // Create helper function
-
 const pivot = (arr, start = 0, end = arr.length - 1) => {
-    // set pivot 
-    let pviot = arr[start];
-    // set swapIdx
-        // this will be used when we swap the pivot index(start) to swapIdx
-    let swapIdx = start;
 
     // Swap helper function with ES6
     const swap = (arr,i ,j) => {
         [arr[i], arr[j]] = [arr[j], arr[i]]
     }
+
+    // set pivot 
+    let pivot = arr[start];
+    // set swapIdx
+        // this will be used when we swap the pivot index(start) to swapIdx
+    let swapIdx = start;
 
     // Iterate over an array
     for (let i = start + 1; i < arr.length; i ++) {
@@ -33,13 +33,18 @@ const pivot = (arr, start = 0, end = arr.length - 1) => {
 
 // Quicksort function
 
-const quicksort = (arr, left = 0, right = arr.length - 1) => {
-    let pivotIndex = pivot(arr, left, right);
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
 
     if (left < right) {
-        quicksort(arr, left, pivotIndex - 1);
-        quicksort(arr, pivotIndex + 1, right);
+        let pivotIndex = pivot(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
     }
 
     return arr;
 }
+
+var a = [-2, 199, - 382, 3, 2, 6, 6, 1, 0, 421];
+var b = [6,5,7,4,8,3,9,2,0,1]
+quickSort(a);
+quickSort(b);
