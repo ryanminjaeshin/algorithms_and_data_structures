88786c7764f55c8d42e192ce7e9a5be290be579a
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-30 22:44:25 -0800

    Finish pivot func

--- algorithms/sorting_quicksort.js
+++ algorithms/sorting_quicksort.js
@@ -12,6 +12,9 @@ const pivot = (arr, start = 0, end = arr.length) => {
     for (let i = start + 1; i < arr.length; i ++) {
         if (pivot > arr[i]) {
             swapIdx ++;
-        }
+            swap(arr, swapIdx, i)
+        } 
     }
+    swap(arr, start, swapIdx)
+    return swapIdx
 }
