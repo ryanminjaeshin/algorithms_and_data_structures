5f8d95dfc386de35549979b8a3e633091d349dee
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-30 16:04:43 -0800

    Finish

---
+++ algorithms/array_remove_duplicate_grokking.js
@@ -0,0 +1,33 @@
+/*
+
+Problem Statement #
+Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
+
+Example 1:
+
+Input: [2, 3, 3, 3, 6, 9, 9]
+Output: 4
+Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+Example 2:
+
+Input: [2, 2, 2, 11]
+Output: 2
+Explanation: The first two elements after removing the duplicates will be [2, 11].
+
+*/
+
+const remove_duplicates = function(arr) {
+    // TODO: Write your code here
+    var nextNonDuplicate = 1;
+    var i = 1;
+
+    while(i < arr.length) {
+        if(arr[nextNonDuplicate - 1] !== arr[i]) {
+            arr[nextNonDuplicate] = arr[i];
+            nextNonDuplicate ++;
+        }
+        i ++;
+    }
+    return nextNonDuplicate;
+  };
+  
