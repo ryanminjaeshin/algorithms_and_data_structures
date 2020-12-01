80e15c737dfd84e1e893fe7533daa8c501c66e2f
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-30 22:52:52 -0800

    Add pseudo code WIP

--- algorithms/sorting_quicksort.js
+++ algorithms/sorting_quicksort.js
@@ -12,6 +12,7 @@ const pivot = (arr, start = 0, end = arr.length) => {
         [arr[i], arr[j]] = [arr[j], arr[i]]
     }
 
+    // Iterate over an array
     for (let i = start + 1; i < arr.length; i ++) {
         if (pivot > arr[i]) {
             swapIdx ++;
