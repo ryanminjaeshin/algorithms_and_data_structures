06a6711a2314a35cca4d913d5932d2cdd871fc71
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-11-30 22:39:07 -0800

    WIP pivot func

---
+++ algorithms/sorting_quicksort.js
@@ -0,0 +1,17 @@
+// Create helper function
+
+const pivot = (arr, start = 0, end = arr.length) => {
+    let pviot = arr[start];
+    let swapIdx = start;
+
+    const swap = (arr,i ,j) => {
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+
+
+    for (let i = start + 1; i < arr.length; i ++) {
+        if (pivot > arr[i]) {
+            swapIdx ++;
+        }
+    }
+}
