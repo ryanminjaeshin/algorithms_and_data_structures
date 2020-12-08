71307adcaa59a6aa955adefbf33340443a1c98e5
ryanminjaeshin <ryanminjaeshin@gmail.com>
2020-12-07 18:49:39 -0800

    Add solution

--- algorithms/array_triple_sum_to_zero_grokking.js
+++ algorithms/array_triple_sum_to_zero_grokking.js
@@ -16,29 +16,38 @@ Explanation: There are two unique triplets whose sum is equal to zero.
 
 */
 
-const search_triplets = function(arr) {
-  // TODO: Write your code here
-
-  let result = [];
-  const len = arr.length;
-
-  for (let p1 = 0; p1 < len; p1 += 1) {
-
-    let p2 = p1 + 1;
-
-    while (p2 < len) {
-
-      let twoSum = arr[p1] + arr[p2];
-
-      var target = - twoSum;
-
-      if (arr.slice(p2 + 1).indexOf(target) > -1) {
-        result.push([arr[p1], arr[p2], target])
-      }
-
-      p2 += 1;
-    }
-  }
-
-  return result;
+var threeSum = function(nums) {
+	var rtn = [];
+	if (nums.length < 3) {
+		return rtn;
+	}
+	nums = nums.sort(function(a, b) {
+		return a - b;
+	});
+	for (var i = 0; i < nums.length - 2; i++) {
+		if (nums[i] > 0) {
+			return rtn;
+		}
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+		for (var j = i + 1, k = nums.length - 1; j < k;) {
+			if (nums[i] + nums[j] + nums[k] === 0) {
+				rtn.push([nums[i], nums[j], nums[k]]);
+				j++;
+				k--;
+				while (j < k && nums[j] == nums[j - 1]) {
+					j++;
+				}
+				while (j < k && nums[k] == nums[k + 1]) {
+					k--;
+				}
+			} else if (nums[i] + nums[j] + nums[k] > 0) {
+				k--;
+			} else {
+				j++;
+			}
+		}
+	}
+	return rtn;
 };
