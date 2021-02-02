6cdc286920c35d8497f6fcebdd6c62ec29545bd6
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-02 14:13:31 -0800

    Add insert method

--- data_structures/trie_leetcode_208.js
+++ data_structures/trie_leetcode_208.js
@@ -4,4 +4,14 @@ class Trie {
   constructor() {
     this.root = {};
   }
+
+  insert(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (node[char] === null) node[char] = {};
+      node = node[char];
+    }
+
+    node.isWord = true;
+  }
 }
