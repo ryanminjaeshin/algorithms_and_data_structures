f4504795f4f70f390239ae77de5328fea003d9fc
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-02 15:47:03 -0800

    Solve dropbox problem

---
+++ algorithms/array_dropbox_summer_internship.js
@@ -0,0 +1,12 @@
+const findDatabase = (arr, k) => {
+  let windowEnd = k;
+  let newBucket = [];
+
+  for (let windowStart = 0; windowStart < arr.length - k + 1; windowStart += 1) {
+    let bucket = arr.slice(windowStart, windowEnd)
+    newBucket.push(Math.min(...bucket));
+    windowEnd += 1;
+  }
+
+  return Math.max(...newBucket);
+}
