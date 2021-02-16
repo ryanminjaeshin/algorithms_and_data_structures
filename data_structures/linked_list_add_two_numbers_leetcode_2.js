8c6dc37ad97f15c53d8833e16fb348010d3b151b
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-02-15 20:46:11 -0800

    Solved

--- data_structures/linked_list_add_two_numbers_leetcode_2.js
+++ data_structures/linked_list_add_two_numbers_leetcode_2.js
@@ -32,5 +32,29 @@
 
 
 var addTwoNumbers = function(l1, l2) {
+    var p1 = l1;
+    var p2 = l2;
+    var carry = 0;
+    var p3 = new ListNode(0);
+    var result = p3;
     
+    while (p1 !== null || p2 !== null) {
+        var x = (p1 !== null) ? p1.val : 0;
+        var y = (p2 !== null) ? p2.val : 0;
+        
+        var sum = x + y + carry;
+        
+        carry = Math.floor(sum / 10);
+
+        result.next = new ListNode(sum % 10);
+        
+        result = result.next;
+        
+        if (p1 !== null) p1 = p1.next;
+        if (p2 !== null) p2 = p2.next;
+    }
+    
+    if (carry > 0) result.next = new ListNode(1);
+    
+    return p3.next;
 };
