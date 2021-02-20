8be13245c13c07102b281529fdecf1c41d8fc952
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-20 14:03:32 -0800

    Recap [2] 2/20

--- algorithms/array_remove_duplicate_grokking.js
+++ algorithms/array_remove_duplicate_grokking.js
@@ -52,16 +52,17 @@ const remove_duplicates = function(arr) {
 
   const remove_duplicates = function(arr) {
     // TODO: Write your code here
-    let nextNonDuplicate = 1;
+    let nextNonDup = 1;
+    let i  = 1;
 
-    let i = 1;
     while (i < arr.length) {
-      if (arr[nextNonDuplicate - 1] !== arr[i]) {
-        arr[nextNonDuplicate] = arr[i];
-        nextNonDuplicate += 1;
+      if (arr[nextNonDup - 1] !== arr[i]) {
+        arr[nextNonDup] = arr[i];
+        nextNonDup += 1;
       }
+
       i += 1;
     }
 
-    return nextNonDuplicate;
+    return nextNonDup;
   };
