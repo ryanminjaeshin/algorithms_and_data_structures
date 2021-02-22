947404f46dfa1c81e48a17431624860c43c4286b
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-22 15:21:53 -0800

    Add example

--- data_structures/binary_search_tree_order_traversal_II_leetcode_107.js
+++ data_structures/binary_search_tree_order_traversal_II_leetcode_107.js
@@ -12,6 +12,27 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+
+ /*
+
+ Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+
+*/
+
+
 var levelOrderBottom = function(root) {
 
 };
