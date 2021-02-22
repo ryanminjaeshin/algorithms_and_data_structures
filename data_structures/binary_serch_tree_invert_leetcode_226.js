84d509e798122dd73056a3bd21c17d3e65e7c9e9
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-22 15:42:19 -0800

    Add prompt & example

---
+++ data_structures/binary_serch_tree_invert_leetcode_226.js
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/invert-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ *
+ * Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ */
+var invertTree = function(root) {
+
+};
