c1fb0b012c79dea2a64f7da5c26269b52ff8e8e8
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-11 19:50:38 -0800

    Solved

--- algorithms/string_maximum-length-of-a-concatenated-string-with-unique-characters_leetcode_1239.js
+++ algorithms/string_maximum-length-of-a-concatenated-string-with-unique-characters_leetcode_1239.js
@@ -23,3 +23,23 @@ Example 3:
 Input: arr = ["abcdefghijklmnopqrstuvwxyz"]
 Output: 26
 */
+
+var maxLength = function (arr) {
+  let res = 0;
+
+  let solve = (start, curr) => {
+      if (curr.length !== new Set(curr.split('')).size) {
+          return;
+      }
+
+      res = Math.max(res, curr.length);
+
+      for (let i = start; i < arr.length; i++) {
+          solve(i + 1, `${curr}${arr[i]}`);
+      }
+  }
+
+  solve(0, '');
+
+  return res;
+};
