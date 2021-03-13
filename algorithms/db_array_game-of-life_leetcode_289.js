69fcad5714774ad72b0cf961615d49c9d4eabd0f
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-13 14:37:47 -0800

    Mark the board

--- algorithms/db_array_game-of-life_leetcode_289.js
+++ algorithms/db_array_game-of-life_leetcode_289.js
@@ -12,14 +12,32 @@ Any live cell with more than three live neighbors dies, as if by over-population
 Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
 
-
-
 */
 
 /**
  * @param {number[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
- var gameOfLife = function(board) {
 
+//  I
+//  O
+//  C
+//  E
+
+ var gameOfLife = function(board) {
+   // mark live -> die as -1
+   // mark die -> live as 2
+
+   let row = board.length;
+   let col = board[0].length;
+
+   // Mark the board
+   for (let i = 0; i < row; i += 1) {
+     for (let j = 0; j < col; j += 1) {
+       let cell = board[i][j];
+       let neighbors = getNeighbors(i, j, board);
+       if (cell == 1 && (neighbors < 2 || neighbors > 3)) board[i][j] = -1;
+       if (cell == 0 && neighbors == 3) board[i][j] = 2;
+     }
+   }
 };
