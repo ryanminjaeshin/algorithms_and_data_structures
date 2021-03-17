33e6cdc7682bf2cf27cf2d1c7612d3ecc06064a9
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-03-16 22:05:56 -0700

    Solved

---
+++ algorithms/sorting_insertionsort.js
@@ -0,0 +1,14 @@
+const insertionSort = (arr) => {
+
+    for (let i = 0; i < arr.length - 1; i ++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j ++) {
+            if(arr[j] < arr[minIndex]) minIndex = j;
+        }
+
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+    return arr;
+}
+
+// t -> O(n * n)
