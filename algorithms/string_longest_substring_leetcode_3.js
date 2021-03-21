758c8bf010c0d7227c214a01b1038ddcc4c8996c
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-20 17:05:50 -0700

    recap [2] 3/20

--- algorithms/string_longest_substring_leetcode_3.js
+++ algorithms/string_longest_substring_leetcode_3.js
@@ -63,17 +63,43 @@ lengthOfLongestSubstring('dvdf')
 var lengthOfLongestSubstring = (s) => {
     var substring = '';
     var max = 0;
-    
+
     for (var i = 0; i < s.length; i ++) {
         var index = substring.indexOf(s[i]);
         substring += s[i];
-        
+
         if (index >= 0) {
             substring = substring.slice(index + 1);
         }
-        
+
         max = Math.max(substring.length, max)
     }
-    
+
     return max;
 };
+
+var lengthOfLongestSubstring = function(s) {
+    let map = new Map();
+    let max = 0;
+    let len = s.length;
+    let windowStart = 0;
+
+    for (let windowEnd = 0; windowEnd < len; windowEnd ++) {
+        let right = s[windowEnd];
+
+        if (!map[right]) map[right] = 0;
+        map[right] ++;
+
+        while(Object.values(map).indexOf(2) > -1) {
+            let left = s[windowStart];
+            map[left] --;
+            windowStart ++;
+        }
+
+        max = Math.max(max, windowEnd - windowStart + 1)
+    }
+
+    return max;
+};
+
+// Recap
