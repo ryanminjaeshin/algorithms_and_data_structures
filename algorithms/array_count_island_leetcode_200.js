03692c66d0caa38f667576dd91550462f997a1d8
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-22 21:52:41 -0700

    recap [2] 3/22

--- algorithms/array_count_island_leetcode_200.js
+++ algorithms/array_count_island_leetcode_200.js
@@ -1,10 +1,10 @@
 /** https://leetcode.com/problems/number-of-islands/
- * 
+ *
     Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
     An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
-    
+
 
     Example 1:
 
@@ -47,3 +47,27 @@ const check = (row, col, grid) => {
     check(row + 1, col, grid);
     check(row - 1, col, grid);
 }
+
+// recap
+
+var numIslands = function(grid) {
+    let count = 0;
+    for (let row = 0; row < grid.length; row ++) {
+        for (let col = 0; col < grid[row].length; col ++) {
+            if(grid[row][col] == 1) {
+                count ++;
+                check(grid, row, col)
+            }
+        }
+    }
+    function check (grid, r, c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[r].length || grid[r][c] == 0) return;
+        grid[r][c] = 0;
+        check(grid, r - 1, c);
+        check(grid, r , c - 1);
+        check(grid, r + 1, c);
+        check(grid, r, c + 1)
+    }
+
+    return count;
+};
