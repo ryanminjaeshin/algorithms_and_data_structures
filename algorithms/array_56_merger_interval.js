d689f9faad4f45f52041cfc1a7037ba38b29d94c
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-25 11:16:03 -0700

    solved

---
+++ algorithms/array_56_merger_interval.js
@@ -0,0 +1,15 @@
+function merge(intervals) {
+  if (!intervals.length) return intervals
+  intervals.sort((a, b) => a.start !== b.start ? a.start - b.start : a.end - b.end)
+  var prev = intervals[0]
+  var res = [prev]
+  for (var curr of intervals) {
+    if (curr.start <= prev.end) {
+      prev.end = Math.max(prev.end, curr.end)
+    } else {
+      res.push(curr)
+      prev = curr
+    }
+  }
+  return res
+}
