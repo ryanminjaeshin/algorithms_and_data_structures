c5071bbcd214466b309993e234436b5ed59c5989
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-27 18:59:26 -0700

    recap [1] 3/27

--- algorithms/array_33_search-in-rotated-sorted-array.js
+++ algorithms/array_33_search-in-rotated-sorted-array.js
@@ -30,3 +30,42 @@ var search = function(nums, target) {
 
   return -1;
 };
+
+var search = function(nums, target) {
+    if (nums == null || nums.length === 0) return -1;
+
+    let l = 0,
+        r = nums.length - 1;
+
+    while (l < r) {
+        let m = Math.floor((l + r) / 2);
+        if (nums[r] < nums[m]) l = m + 1;
+        else r = m;
+    }
+
+    let start = l;
+    l = 0;
+    r = nums.length - 1;
+
+
+    if (target >= nums[start] && target <= nums[r]) {
+        l = start;
+    } else {
+        r = start;
+    }
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (nums[m] === target) {
+            return m;
+        } else if (nums[m] < target) {
+            l = m  + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    return -1;
+};
+
+// recap
