37ea5b80e85a21d4383c19d9b0667919665d7a89
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-27 22:27:33 -0700

    solved

---
+++ algorithms/array_matrix_73_set-matrix-zeroes.js
@@ -0,0 +1,30 @@
+var setZeroes = function(matrix) {
+  let row = matrix.length,
+      col = matrix[0].length
+
+  for (let r = 0; r < row; r ++) {
+      for (let c = 0; c < col; c ++) {
+          if(matrix[r][c] === 0) {
+              setMatrix(matrix, r, c)
+          }
+      }
+  }
+
+  for (let r = 0; r < row; r ++) {
+      for (let c = 0; c < col; c++) {
+          if(matrix[r][c] === true) matrix[r][c] = 0;
+      }
+  }
+  return matrix;
+};
+
+const setMatrix = (matrix, r, c) => {
+
+  for (let i = 0; i < matrix[0].length; i ++) {
+      if(matrix[r][i] !== 0) matrix[r][i] = true;
+  }
+  for (let i = 0; i < matrix.length; i ++) {
+      if(matrix[i][c] !== 0) matrix[i][c] = true;
+  }
+  matrix[r][c] = 0;
+}
