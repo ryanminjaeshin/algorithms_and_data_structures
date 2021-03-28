052cf23789097a676bd1f49edaed69c51882943d
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-27 23:05:55 -0700

    solved

---
+++ algorithms/array_matrix_54_spiral.js
@@ -0,0 +1,34 @@
+var spiralOrder = function(matrix) {
+  let res = new Array();
+  let rb = 0,
+      re = matrix.length - 1,
+      cb = 0,
+      ce = matrix[0].length - 1;
+
+  while (rb <= re && cb <= ce) {
+      for (let i = cb; i <= ce; i ++) {
+          res.push(matrix[rb][i]);
+      }
+      rb ++;
+      for (let i = rb; i <= re; i ++) {
+          res.push(matrix[i][ce]);
+      }
+      ce --;
+
+      if (rb > re || cb > ce) break;
+
+      for (let i = ce; i >= cb; i --) {
+          res.push(matrix[re][i])
+      }
+
+      re --;
+
+      for (let i = re; i >= rb; i --) {
+          res.push(matrix[i][cb])
+      }
+
+      cb ++;
+  }
+
+  return res;
+};
