f4f85ae60e8367e1246e87f683c38edd5cd7350b
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-03-29 16:31:04 -0700

    solved

---
+++ data_structures/binary_tree_94_binary-tree-inorder-traversal.js
@@ -0,0 +1,19 @@
+var inorderTraversal = function(root) {
+    let result = [];
+
+   const traverse = node => {
+     
+     if (!node) return;
+     
+     if (node.left) traverse(node.left)
+
+     result.push(node.val)
+
+     if (node.right) traverse(node.right)
+
+   }
+
+   traverse(root)
+
+   return result
+};
