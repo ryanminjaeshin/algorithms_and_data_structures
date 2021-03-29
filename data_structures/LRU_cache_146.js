cbad9f8d9415bb52d9c9b49b6f2b96fe27e03a1b
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-03-29 15:42:45 -0700

    recap[2] 3/28

--- data_structures/LRU_cache_146.js
+++ data_structures/LRU_cache_146.js
@@ -61,3 +61,27 @@ class LRUCache {
 }
 
 // Recap
+
+class LRUCache {
+  constructor(capacity) {
+    this.map = new Map();
+    this.capacity = capacity;
+  }
+
+  get(key) {
+    if(!this.map.has(key)) return -1;
+    const val = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, val);
+    return val; 
+  }
+
+  put(key, val) {
+    this.map.delete(key);
+    this.map.set(key, val);
+    if (this.map.size > this.capacity) {
+      const first = this.map.keys().next().value;
+      this.map.delete(first);
+    }
+  }
+}
