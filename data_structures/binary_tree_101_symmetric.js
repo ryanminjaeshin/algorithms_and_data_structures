397d89456bc0317022cd5ba34018bcd20b1d206f
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-03-29 16:50:23 -0700

    solved

---
+++ data_structures/binary_tree_101_symmetric.js
@@ -0,0 +1,10 @@
+var isSymmetric = function(root) {
+    return isMirror(root, root);
+};
+
+const isMirror = (t1, t2) => {
+    if(!t1 && !t2) return true;
+    if(!t1 || !t2) return false;
+    
+    return (t1.val === t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left)
+}
