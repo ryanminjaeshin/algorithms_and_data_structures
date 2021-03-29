9f734f4804bd125343f0b082ad347181b764bbda
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-03-29 12:35:28 -0700

    solved

---
+++ algorithms/string_49_group_anagrams.js
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/group-anagrams/
+var groupAnagrams = function(strs) {
+    let map = {},
+        result = [];
+    for (let i = 0; i < strs.length; i ++) {
+        let str = strs[i]
+        let sorted = str.split('').sort().join('');
+        if (!map[sorted]) {
+            map[sorted] = [str]
+        } else {
+            map[sorted].push(str);
+        }
+    }
+    
+    for (let prop in map) {
+        result.push(map[prop])
+    }
+    
+    return result;
+};
