4f14c8cd357d79b52f80cf9e67d92f21eb3bf093
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-03-29 17:13:11 -0700

    recap [1] 3/29

---
+++ data_structures/binary_tree_112_path_sum_grokking.js
@@ -0,0 +1,40 @@
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+
+function hasPath(root, sum) {
+    if(!root) false;
+    if(root.val === sum && !root.left & !root.right) true;
+    return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val);
+}
+
+var hasPathSum = function(root, targetSum) {
+  if (!root) return false;
+  let rootStack = [root];
+  let sumStack = [targetSum - root.val];
+  while(rootStack.length > 0) {
+      
+      let currentNode = rootStack.pop();
+      let currentSum = sumStack.pop();
+      
+      if (!currentNode.left && !currentNode.right && currentSum === 0) {
+          return true;
+      }
+      
+      if (currentNode.left) {
+          rootStack.push(currentNode.left);
+          sumStack.push(currentSum - currentNode.left.val)
+      }
+      if (currentNode.right) {
+          rootStack.push(currentNode.right);
+          sumStack.push(currentSum - currentNode.right.val)
+      }
+  }
+  return false;
+  
+};
