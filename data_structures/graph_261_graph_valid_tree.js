8af2e09912bc50c96a71289a470190f1eb0dfbce
Ryan Shin <ryanminjaeshin@gmail.com>
2021-04-01 22:48:46 -0700

    recap [1] 4/1

--- data_structures/graph_261_graph_valid_tree.js
+++ data_structures/graph_261_graph_valid_tree.js
@@ -47,3 +47,47 @@ const isCycle = (node, graph, visited, parent) => {
   }
   return false;
 }
+
+
+// recap
+
+var validTree = function(n, edges) {
+    let graph = buildGraph(n, edges);
+    let visited = new Set();
+    // check cycle
+    if (dfs(0, -1, graph, visited)) return false;
+    // check unconnected component
+    for (let i = 0; i < n; i ++) {
+        if(!visited.has(i)) return false;
+    }
+
+    return true;
+};
+
+const buildGraph = (n, edges) => {
+    let graph = Array.from({length: n}, () => []);
+    for (edge of edges) {
+        let [src, dest] = edge;
+        graph[src].push(dest);
+        graph[dest].push(src);
+    }
+    return graph;
+}
+
+const dfs = (current, parent, graph, visited) => {
+    // add current node to visited
+    visited.add(current);
+    // check neighbors of current node
+    for (let neighbor of graph[current]) {
+        // haven't seen this node before
+        if (!visited.has(neighbor)) {
+            // check this node has cycle
+            if(dfs(neighbor, current, graph, visited)) return false;
+        } else {
+            // if we seen this neighbor before => it should be not equal to the parent (if it is, it has cycle)
+            if (neighbor !== parent) return true;
+        }
+    }
+    return false;
+
+}
