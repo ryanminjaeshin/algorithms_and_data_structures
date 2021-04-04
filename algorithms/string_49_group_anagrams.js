95848c3efa4d073e99bac80cd929442b2d353b72
Ryan Shin <ryanminjaeshin@gmail.com>
2021-04-03 20:56:00 -0700

    recap [2] 4/3

--- algorithms/string_49_group_anagrams.js
+++ algorithms/string_49_group_anagrams.js
@@ -11,10 +11,27 @@ var groupAnagrams = function(strs) {
             map[sorted].push(str);
         }
     }
-    
+
     for (let prop in map) {
         result.push(map[prop])
     }
-    
+
     return result;
 };
+
+var groupAnagrams = function(strs) {
+    let res = [],
+        map = new Map();
+    for (let i = 0; i < strs.length; i ++) {
+        let str = strs[i];
+        let sorted = str.split('').sort().join('');
+        if (!map[sorted]) {
+            map[sorted] = [str];
+        } else {
+            map[sorted].push(str);
+        }
+     }
+    return Object.values(map)
+};
+
+// recap
