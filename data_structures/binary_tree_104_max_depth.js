c0e40e69d051458bc7f74c78683d295fb06aafbe
Ryan Shin <ryanminjaeshin@gmail.com>
2021-04-04 16:31:03 -0700

    recap [2] 4/4

--- data_structures/binary_tree_104_max_depth.js
+++ data_structures/binary_tree_104_max_depth.js
@@ -8,3 +8,29 @@ var maxDepth = function(root) {
   if(!root) return 0;
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 };
+
+var maxDepth = function(root) {
+  if (!root) return 0;
+
+  let nodeStack = [root];
+  let depthStack = [1];
+  let maxDepth = 0;
+
+  while (nodeStack.length) {
+      let currentNode = nodeStack.pop();
+      let currentDepth = depthStack.pop();
+      maxDepth = Math.max(maxDepth, currentDepth);
+
+      if (currentNode.left) {
+          nodeStack.push(currentNode.left);
+          depthStack.push(currentDepth + 1)
+      }
+
+      if (currentNode.right) {
+          nodeStack.push(currentNode.right);
+          depthStack.push(currentDepth + 1)
+      }
+  }
+
+  return maxDepth;
+};
