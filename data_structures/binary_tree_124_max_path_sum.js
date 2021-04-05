c7459d72ce77016acee6cd6b36564f2d94a2cab1
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-04 18:43:36 -0700

    solved

---
+++ data_structures/binary_tree_124_max_path_sum.js
@@ -0,0 +1,13 @@
+var maxPathSum = function(root) {
+    let max = -Infinity;
+
+    const  pathSum = root => {
+        if(!root) return 0;
+        let left = Math.max(0, pathSum(root.left));
+        let right = Math.max(0, pathSum(root.right));
+        max = Math.max(left + right + root.val, max);
+        return Math.max(left, right) + root.val;
+    }
+    pathSum(root);
+    return max;
+};
