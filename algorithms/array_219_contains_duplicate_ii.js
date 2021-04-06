056215344bfebb9fd7026b4b70b65966c0671b4e
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-05 23:27:39 -0700

    solved

---
+++ algorithms/array_219_contains_duplicate_ii.js
@@ -0,0 +1,8 @@
+var containsNearbyDuplicate = function(nums, k) {
+    let map = {};
+    for (let i = 0; i < nums.length; i ++) {
+        if (i - map[nums[i]] <= k) return true;
+            map[nums[i]] = i;
+    }
+    return false;
+}
