5d470d93f125195545dcc4dfdebf2742e7c1923c
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-12 17:24:31 -0700

    solved

---
+++ data_structures/binary_tree_113_path_sum_2.js
@@ -0,0 +1,13 @@
+var pathSum = function(root, targetSum) {
+    let res = [];
+    const helper = (node, targetSum, curr) => {
+    if(!node) return;
+    targetSum -= node.val;
+    curr.push(node.val);
+    if(!node.left && !node.right && targetSum === 0) res.push(curr) 
+    node.left = helper(node.left, targetSum, [...curr]);
+    node.right = helper(node.right, targetSum, [...curr])
+}
+    helper(root, targetSum, []);
+    return res;
+};
