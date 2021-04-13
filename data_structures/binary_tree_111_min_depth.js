336030c4e35d07c2edfc99b5500cde1357bdcb19
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-13 13:28:20 -0700

    solved

---
+++ data_structures/binary_tree_111_min_depth.js
@@ -0,0 +1,15 @@
+var minDepth = function(root) {
+    if (!root) return 0;
+    let depth = 1,
+        queue = [root];
+    while (queue.length) {
+        let levelSize = queue.length;
+        for (let i = 0; i < levelSize; i ++) {
+            let currentNode = queue.shift();
+            if (!currentNode.left && !currentNode.right) return depth
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
+        }
+        depth ++
+    }
+};
