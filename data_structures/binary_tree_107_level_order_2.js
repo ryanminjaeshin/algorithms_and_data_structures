21098c90ee4a8e3a4dedba934577965cab98a78d
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-13 13:28:59 -0700

    solved

---
+++ data_structures/binary_tree_107_level_order_2.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ var levelOrderBottom = function(root) {
+    if (!root) return [];
+    let queue = [root],
+        res = [];
+    while (queue.length) {
+        let currentLevel = [];
+        let currentSize = queue.length;
+        for (let i = 0; i < currentSize; i ++) {
+            let currentNode = queue.shift();
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
+            currentLevel.push(currentNode.val);
+        }
+        res.unshift(currentLevel)
+    }
+    return res;
+};
