08cd58ea0d35be40e776ec525cc1ec2b157b4684
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-14 11:37:00 -0700

    solved

---
+++ algorithms/array_435_intervals_remove.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+ var eraseOverlapIntervals = function(intervals) {
+    let count = 0;
+    if (!intervals.length) return count;
+    intervals.sort((a, b) => a[1] - b[1]);
+    let prev = intervals[0];
+    for (let i = 1; i < intervals.length; i ++) {
+        if (prev[1] > intervals[i][0]) count ++
+        else prev = intervals[i]
+    }
+    return count;
+};
