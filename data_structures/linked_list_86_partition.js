58b424a67b62bcbc003afafc02034923a6fe7089
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-14 18:21:50 -0700

    solved

---
+++ data_structures/linked_list_86_partition.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+ var partition = function(head, x) {
+    let afterHead = new ListNode(0),
+        after = afterHead,
+        beforeHead = new ListNode(0),
+        before = beforeHead;
+    while(head) {
+        if (head.val < x) {
+            before.next = head;
+            before = before.next;
+        } else {
+            after.next = head;
+            after = after.next;
+        }
+        head = head.next;
+    }
+
+    after.next = null;
+    before.next = afterHead.next;
+    return beforeHead.next;
+};
