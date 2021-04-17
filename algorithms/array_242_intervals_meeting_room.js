4c7e0fa2aa0eb1dd8b28bd9beca44ce83c3631a5
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-17 13:40:31 -0700

    solved

---
+++ algorithms/array_242_intervals_meeting_room.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+ var canAttendMeetings = function(intervals) {
+    if (intervals.length <= 1) return true;
+    intervals.sort((a, b) => a[0] - b[0])
+    for (let i = 0; i < intervals.length - 1; i ++) {
+        if (intervals[i][1] > intervals[i + 1][0]) {
+            return false;
+        }
+    }
+    return true;
+};
