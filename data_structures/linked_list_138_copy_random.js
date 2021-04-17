e435c6518618d24be4ca44ecca3a24085f13a5d9
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-16 19:20:24 -0700

    solved

---
+++ data_structures/linked_list_138_copy_random.js
@@ -0,0 +1,17 @@
+var copyRandomList = function(head) {
+    if (!head) return head;
+    let map = new Map();
+    let tmp = head;
+    // deep copy to map
+    while(tmp) {
+        map.set(tmp, new Node(tmp.val, null, null));
+        tmp = tmp.next;
+    }
+    tmp = head;
+    while(tmp) {
+        map.get(tmp).next = tmp.next !== null ? map.get(tmp.next) : null;
+        map.get(tmp).random = tmp.random !== null ? map.get(tmp.random) : null;
+        tmp = tmp.next;
+    }
+    return map.get(head);
+};
