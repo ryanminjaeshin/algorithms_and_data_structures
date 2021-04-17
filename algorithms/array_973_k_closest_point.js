3a5f4f54671bb47f1267a4eb98e1017890215230
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-17 14:59:50 -0700

    solved

---
+++ algorithms/array_973_k_closest_point.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+ var kClosest = function(points, k) {
+    let getDistance = a => (a[0] **2) + (a[1] **2)
+    return points.sort((a, b) => getDistance(a) - getDistance(b)).slice(0, k)
+};
