8fcbe624e4ac4e9ce3e4f8bf8a28b92a0f00e36b
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-21 14:58:33 -0700

    solved

---
+++ algorithms/array_704_binary_search.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    let lo = 0;
+    let high = nums.length - 1;
+    while (lo < high) {
+        let mid = lo + Math.floor((high - lo + 1) / 2);
+        if (target < nums[mid]) {
+            high = mid - 1
+        } else {
+            lo = mid;
+        }
+    }
+    return nums[lo] === target ? lo : -1;
+};
