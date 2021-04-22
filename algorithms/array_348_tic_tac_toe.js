7ccc2023ebd47c79c4f456faeaa2cd5ebfc850c9
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-21 20:58:35 -0700

    solved

---
+++ algorithms/array_348_tic_tac_toe.js
@@ -0,0 +1,49 @@
+/**
+ * Initialize your data structure here.
+ * @param {number} n
+ */
+ var TicTacToe = function(n) {
+    this.len = n;
+    this.row = new Array(n).fill(0);
+    this.col = new Array(n).fill(0);
+    this.dia = 0;
+    this.antiDia = 0;
+};
+
+/**
+ * Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. 
+ * @param {number} row 
+ * @param {number} col 
+ * @param {number} player
+ * @return {number}
+ */
+TicTacToe.prototype.move = function(row, col, player) {
+    const i = player === 1 ? 1 : -1;
+    this.row[row] += i;
+    this.col[col] += i;
+    if(row === col) this.dia += i;
+    if(col === this.len - row - 1) this.antiDia += i;
+    
+    if (
+        Math.abs(this.row[row]) == this.len ||
+        Math.abs(this.col[col]) == this.len ||
+        Math.abs(this.dia) == this.len ||
+        Math.abs(this.antiDia) == this.len
+    ) return player;
+    
+    return 0;
+        
+};
+
+/** 
+ * Your TicTacToe object will be instantiated and called as such:
+ * var obj = new TicTacToe(n)
+ * var param_1 = obj.move(row,col,player)
+ */
