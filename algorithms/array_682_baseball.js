daac2707c1d858f7fa26f2921c59647c3f732889
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-22 17:42:09 -0700

    solved

---
+++ algorithms/array_682_baseball.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+ var calPoints = function(ops) {
+    let stack = [];
+    for (let i = 0; i < ops.length; i ++) {
+        let op = ops[i];
+        if (op == "D") {
+            stack.push(stack[stack.length - 1] * 2)
+        } else if (op == 'C') {
+            stack.pop();
+        } else if (op == '+') {
+            stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+        } else {
+            stack.push(Number(op))
+        }
+    }
+    return stack.reduce((a,b) => a + b)
+};
