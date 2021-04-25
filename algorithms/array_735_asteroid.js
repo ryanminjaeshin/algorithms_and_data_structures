7f713e6ac5fd258da505ef3b4ed6e05f63509430
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-25 15:09:46 -0700

    solved

--- algorithms/array_735_asteroid.js
+++ algorithms/array_735_asteroid.js
@@ -1,1 +1,36 @@
 // amazon, stack
+
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+ var asteroidCollision = function(asteroids) {
+    let stack = [];
+    for (let i = 0; i < asteroids.length; i ++) {
+        let a = asteroids[i];
+        if (stack.length === 0 || a > 0) {
+            stack.push(a)
+        } else {
+            while (true) {
+                let top = stack[stack.length - 1];
+                if (top < 0) {
+                    stack.push(a)
+                    break;
+                } else if (top == -a) {
+                    stack.pop();
+                    break;
+                } else if (top > -a) {
+                    break;
+                } else {
+                    stack.pop();
+                    if (stack.length == 0) {
+                        stack.push(a);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return stack;
+};
+    
