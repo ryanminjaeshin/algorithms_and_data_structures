d9f63368d6b960419197da9a333168f244c0b456
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-27 17:21:59 -0700

    solved

---
+++ data_structures/binary_tree_116_next_pointer.js
@@ -0,0 +1,30 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+ var connect = function(root) {
+    if (!root) return null;
+    let queue = [root];
+    while (queue.length) {
+        let size = queue.length;
+        for (let i = 0; i < size; i ++) {
+            let currentNode = queue.shift();
+            currentNode.next = (i===size-1) ? null:queue[0]
+            if (currentNode.left) {
+                queue.push(currentNode.left);
+                queue.push(currentNode.right)
+            }         
+        }
+    }
+    return root;
+};
