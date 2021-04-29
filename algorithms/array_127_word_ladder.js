45009ac7cc412cac63c2284b19b9b818d3f58a71
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-04-28 17:31:06 -0700

    add keyword

--- algorithms/array_127_word_ladder.js
+++ algorithms/array_127_word_ladder.js
@@ -1,3 +1,4 @@
+// amazon, bfs
 /**
  * @param {string} beginWord
  * @param {string} endWord
@@ -28,3 +29,34 @@
     }
     return 0;
 };
+
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+ var ladderLength = function(beginWord, endWord, wordList) {
+    let res = 1;
+    let queue = [beginWord];
+    let set = new Set(wordList);
+    while (queue.length) {
+        let size = queue.length;
+        for (let i = 0; i < size; i ++) {
+            let currentWord = queue.shift();
+            if (currentWord == endWord) return res;
+            for (let i = 0; i < currentWord.length; i ++) {
+                for (let j = 0; j < 26; j ++) {
+                    let nextWord = currentWord.slice(0, i) + String.fromCharCode(j + 97) + currentWord.slice(i + 1)
+                    if (set.has(nextWord)) {
+                        queue.push(nextWord);
+                        set.delete(nextWord)
+                    }
+                }
+            }
+        }
+        res +=1;
+    }
+    return 0;
+};
+
