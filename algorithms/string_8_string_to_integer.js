d25699f82d35549511c0a79fef5e02236fd90770
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-05-01 15:07:08 -0700

    solved

---
+++ algorithms/string_8_string_to_integer.js
@@ -0,0 +1,19 @@
+// amazon, atoi
+var myAtoi = function(s) {
+    let i = 0,
+        num = 0,
+        sign = 1,
+        max = 2**31 - 1,
+        min = 2**31 * -1;
+    s = s.trim();
+    if (s[i]=='-' || s[i]=='+') {
+        sign = s[i]=='-'?-1:1;
+        i++
+    }
+    while (s[i] && s[i].charCodeAt(0) - 48 >= 0 && s[i].charCodeAt(0) - 48 <= 9) {
+        num = num * 10 + (s[i].charCodeAt(0) - 48);
+        i ++
+    }
+    num = num * sign;
+    return num <= min ? min : num >= max ? max : num;
+};
