d5566a5805f43a34eedd37332d4835db83781988
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-05-02 19:25:46 -0700

    recap [1]

--- algorithms/array_42_trapping_the_water.js
+++ algorithms/array_42_trapping_the_water.js
@@ -22,3 +22,28 @@
     }
     return res;
 };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var trap = function(height) {
+    let len = height.length;
+    let leftHeight = new Array(len)
+    let rightHeight = new Array(len)
+    let res = 0;
+    leftHeight[0] = height[0];
+    rightHeight[len - 1] = height[len - 1]
+    for (let i = 1; i < len; i ++) {
+        leftHeight[i] = Math.max(leftHeight[i-1], height[i]);
+    }
+    for (let i = len - 2; i >= 0; i --) {
+        rightHeight[i] = Math.max(rightHeight[i+1], height[i]);
+    }
+    for (let i = 1; i < len-1; i ++) {
+        res += Math.min(leftHeight[i], rightHeight[i]) - height[i]
+    }
+    return res;
+};
+
+// recap
