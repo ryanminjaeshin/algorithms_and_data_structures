f6f0f462a57c46389a1d556a9e7d9b4190f98eb3
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-05-19 11:03:31 -0700

    add keywords

--- algorithms/string_22_generate_paren.js
+++ algorithms/string_22_generate_paren.js
@@ -1,3 +1,4 @@
+// amazon, backtracking
 var generateParenthesis = function(n) {
     const res = [];
     
@@ -20,3 +21,22 @@ var generateParenthesis = function(n) {
     }
 };
 // recap
+
+function generateParenthesis(n) {
+    const res = [];
+  
+    function go(l, r, s) {  // l: left remaining, r: right remaining
+      if (l > r) return;  // The number of '(' should be always >= ')'
+  
+      if (l === 0 && r === 0) {
+        res.push(s);
+        return;
+      }
+  
+      if (l > 0) go(l - 1, r, s + '(');
+      if (r > 0) go(l, r - 1, s + ')');
+    }
+  
+    go(n, n, '');
+    return res;
+  }
