e82350cf38ae20ea0ea03bf8638c29bdb5fd8e36
ryanminjaeshin <ryanminjaeshin@gmail.com>
2022-03-28 15:36:46 -0700

    blind 75

--- algorithms/array_53_maximum_subarray.js
+++ algorithms/array_53_maximum_subarray.js
@@ -73,3 +73,12 @@ var maxSubArray = function(nums) {
     return Math.max(...dp)
 };
 // recap
+
+var maxSubArray = function(nums) {
+    let dp = new Array(nums.length);
+    dp[0] = nums[0];
+    for (let i = 1; i < nums.length; i ++) {
+        dp[i] = Math.max(nums[i],nums[i] + dp[i-1])
+    }
+    return Math.max(...dp)
+};
